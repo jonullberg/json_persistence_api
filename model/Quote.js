793c59f5295cdaa97107ecddd59939ac31bb1992
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-05-08 18:42:46 -0700

    added the ability to save as a method to the Quote schema

--- model/Quote.js
+++ model/Quote.js
@@ -1,23 +1,50 @@
 'use strict';
 
-var Quote = module.exports = exports = function(obj) {}
-// this.obj = obj;
-// console.log(obj);
-Quote.prototype.id = '0001';
-Quote.prototype.writer = 'Jonathan';
-Quote.prototype.source = 'my brain';
-Quote.prototype.quoteBody = 'YOLO';
+var fs = require('fs');
+
+var Quote = function(obj) {
+	this.writer = obj.writer;
+	this.source = obj.source;
+	this.quoteBody = obj.quoteBody;
+
+}
 Quote.prototype.save = function(callback) {
-	this.id = id;
-	this.writer = writer;
-	this.source = source;
-	this.quoteBody = quoteBody;
-	fs.writeFile('note' + id, JSON.stringify({writer: writer, source: source, quoteBody: quoteBody}));
-	// if(typeof callback === 'function') {
-	// 	callback();
-	// }
+	if(typeof callback === 'function') {
+		callback();
+	}
+	var saveObj = {};
+	saveObj.writer = this.writer;
+	saveObj.source = this.source;
+	saveObj.quoteBody = this.quoteBody;
+	console.log(saveObj);
+	fs.readdir('./quotes/', function(err, files) {
+		// console.log('your files are ' + files);
+
+		var id = '';
+		if(files.length === undefined) {
+			id = 'quote0001.json';
+		} else if (files.length < 9) {
+			id = 'quote000' + (files.length + 1);
+		} else if (files.length < 99 /*&& files.length >= 10*/) {
+			id = 'quote00' + (files.length + 1);
+		} else if (files.length < 999 /*&& files.length >=100*/) {
+			id = 'quote0' + (files.length + 1);
+		} else {
+			id = 'quote' + (files.length + 1);
+		}
+		var fileName = id + '.json';
+		saveObj.id = id;
+		console.log(saveObj);
+		fs.writeFile('./quotes/' + fileName, JSON.stringify(saveObj), function(err) {
+			if(err) throw err;
+			console.log('You\'ve saved your quote');
+		});
+	});
+	console.log('You saved correctly');
 
 };
 Quote.prototype.find = function() {
-
+	console.log('You\'ve ran the find method');
 };
+
+module.exports = exports = Quote;
--- router/routes.js
+++ router/routes.js
@@ -8,18 +8,18 @@ module.exports = function(router) {
 	router.use(bodyparser.json());
 
 	router.post('/quotes', function(req, res) {
-		console.log(req);
+		// console.log(req);
 
 		// console.log(req.body);
+		// console.log(Quote);
 		// console.log(res);
-		// var newQuote = new Quote(req.body);
-		// newQuote.save(function(err, data) {
-		// 	if(err) {
-		// 		// console.log(err);
-		// 		return res.status(400).json({msg: 'server error'});
-		// 	}
-		// 	console.log('this worked');
-		// res.end();
-		// });
+		var newQuote = new Quote(req.body);
+		newQuote.save(function(err, data) {
+			if(err) {
+				console.log(err);
+				return res.status(400).json({msg: 'server error'});
+			}
+		});
+	res.json(newQuote);
 	});
 };
