0c193bb2e6c9e8eccbab810c11c94092522b948b
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-05-09 17:41:49 -0700

    added ability to save, remove and get quotes to Quote.js

--- model/Quote.js
+++ model/Quote.js
@@ -2,12 +2,24 @@
 
 var fs = require('fs');
 
-var Quote = function(obj) {
-	this.writer = obj.writer;
-	this.source = obj.source;
-	this.quoteBody = obj.quoteBody;
+var Quote = function() {}
 
+function zeroFill( number, width ) {
+  width -= number.toString().length;
+  if ( width > 0 ) {
+    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+  }
+  return number + ""; // always return a string
 }
+
+function idGetter( number, width ) {
+	var number = number;
+	var width = width;
+	var id = zeroFill( number, width );
+	return id;
+}
+
+
 Quote.prototype.save = function(callback) {
 	if(typeof callback === 'function') {
 		callback();
@@ -16,35 +28,65 @@ Quote.prototype.save = function(callback) {
 	saveObj.writer = this.writer;
 	saveObj.source = this.source;
 	saveObj.quoteBody = this.quoteBody;
-	console.log(saveObj);
 	fs.readdir('./quotes/', function(err, files) {
-		// console.log('your files are ' + files);
-
-		var id = '';
-		if(files.length === undefined) {
-			id = 'quote0001.json';
-		} else if (files.length < 9) {
-			id = 'quote000' + (files.length + 1);
-		} else if (files.length < 99 /*&& files.length >= 10*/) {
-			id = 'quote00' + (files.length + 1);
-		} else if (files.length < 999 /*&& files.length >=100*/) {
-			id = 'quote0' + (files.length + 1);
+		if(files[files.length - 1] === undefined) {
+			var id = '0000' + '';
 		} else {
-			id = 'quote' + (files.length + 1);
+			id = idGetter(parseInt(files[files.length - 1].slice(0,4), 10) + 1, 4);
 		}
 		var fileName = id + '.json';
 		saveObj.id = id;
-		console.log(saveObj);
 		fs.writeFile('./quotes/' + fileName, JSON.stringify(saveObj), function(err) {
 			if(err) throw err;
 			console.log('You\'ve saved your quote');
 		});
 	});
-	console.log('You saved correctly');
 
 };
-Quote.prototype.find = function() {
-	console.log('You\'ve ran the find method');
+
+Quote.find = function(obj, callback) {
+	if (obj._id === undefined) {
+		fs.readdir('./quotes/', function(err, files) {
+			for(var i = 0; i < files.length; i++) {
+				fs.readFile('./quotes/' + files[i], function(err, data) {
+					if(err) {
+						console.log(err);
+					}
+					var parsed = data.toString();
+					var jsoned = JSON.parse(parsed);
+					if(typeof callback === 'function') {
+						callback();
+					}
+					console.log(JSON.parse(data.toString()));
+					return data.toString();
+				});
+			}
+		});
+	} else {
+		fs.readFile('./quotes/' + obj._id + '.json', function(err, data) {
+			console.log(JSON.parse(data.toString()));
+			// if(typeof callback === 'function') {
+			// 	callback();
+			// }
+			return JSON.parse(data.toString());
+		});
+	}
 };
+
+Quote.update = function(id, callback) {
+	console.log('You have run the update method on a quote object');
+};
+
+Quote.remove = function(obj, callback) {
+	console.log('You have run the remove method on a quote object');
+	var id = obj._id;
+	console.log(id);
+	fs.unlink('./quotes/' + id + '.json', function(err) {
+		if(err) {
+			console.log(err);
+		}
+	});
+};
+
 
 module.exports = exports = Quote;
--- router/routes.js
+++ router/routes.js
@@ -8,18 +8,53 @@ module.exports = function(router) {
 	router.use(bodyparser.json());
 
 	router.post('/quotes', function(req, res) {
-		// console.log(req);
-
-		// console.log(req.body);
-		// console.log(Quote);
-		// console.log(res);
-		var newQuote = new Quote(req.body);
+		var newQuote = new Quote();
+		console.log(req.body);
+		newQuote.writer = req.body.writer;
+		newQuote.source = req.body.source;
+		newQuote.quoteBody = req.body.quoteBody;
+		console.log(newQuote);
 		newQuote.save(function(err, data) {
 			if(err) {
 				console.log(err);
-				return res.status(400).json({msg: 'server error'});
+				return res.status(500).json({msg: 'internal server error'});
 			}
 		});
 	res.json(newQuote);
+	});
+
+	router.get('/quotes', function(req, res) {
+		Quote.find({}, function(err, data) {
+			if(err) {
+				console.log(err);
+				return res.status(500).json({msg: 'internal server error'});
+			}
+			console.log(data);
+			res.json(data);
+		});
+	});
+
+	router.put('/quotes/:id', function(req, res) {
+		var updatedQuote = req.body;
+		delete updatedQuote._id;
+
+		Quote.update({_id: req.params.id}, updatedQuote, function(err) {
+			if(err) {
+				console.log(err);
+				return res.status(500).json({msg: 'internal server error'});
+			}
+		});
+		res.json({msg: 'success'});
+	});
+
+	router.delete('/quotes/:id', function(req, res) {
+		Quote.remove({_id: req.params.id}, function(err) {
+			if(err) {
+				console.log(err);
+				return res.status(500).json({msg: 'internal server error'});
+			}
+
+		});
+		res.json({msg: 'success'});
 	});
 };
